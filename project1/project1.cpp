96916d08da02be98ee8130fb9fe8b5a395f2ce40
Mansi <mpatel.y@gmail.com>
2016-06-13 17:24:17 -0400

    vector

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -3,10 +3,25 @@
 
 #include "stdafx.h"
 #include "Error.h"
+#include <iostream>
+#include <vector>
 
 
 int main()
 {
+	std::vector<std::string> myvector;
+	std::vector<std::string>::iterator it;
+
+	it = myvector.begin();
+	std::string myarray[] = { "Hi","hello","wassup" };
+	myvector.insert(myvector.begin(), myarray, myarray + 3);
+
+	std::cout << "myvector contains:";
+	for (it = myvector.begin(); it<myvector.end(); it++)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
+
+
 
 	string hi;
 	Error e = Error();
